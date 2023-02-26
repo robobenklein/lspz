406b3cc3eeca465f6ff52c696d72613e7f160aa8
Ben Klein <robobenklein@gmail.com>
2023-02-26 01:25:37 -0500

    help tooltip and popover

--- lspz/entrysite/static/input.js
+++ lspz/entrysite/static/input.js
@@ -258,6 +258,11 @@ function lspz_loaded (event) {
   // finally, start loading the real input:
   lspz_load_samples();
 
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+  const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+  const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 }
 
 window.addEventListener("load", lspz_loaded);
